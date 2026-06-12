cc05ce0ebd501d1a02bf95d713fa66df00b51efe
kavya200731 <kavya200731@gmail.com>
2026-06-12 12:01:54 +0530

    manipulation

---
+++ manipulation.js
@@ -0,0 +1,11 @@
+
+let pen= document.getElementById("wish");
+console.log(pen);
+let mouse= document.getElementByclassname("wishes");
+console.log(mouse);
+let cable= document.getElementBytagname("div");
+console.log(cable);
+let water= document.querySelector("#content");
+console.log(water);
+let queries= document.querySelector("p");
+console.log(queries);
--- script.js
+++ script.js
@@ -1,10 +1,2 @@
- let chocolates=["darkchocolate","melodi","silk","kitkat","fivestar","alphenlebe","perk"];
-let sales={
-    kavya:chocolates[0],
-    navya:chocolates[1],
-    roopa:chocolates[2],
-    hari:chocolates[3],
-    roohi:chocolates[4],
-    thofu:chocolates[5],
-    abhi:chocolates[6]
+le
 }
